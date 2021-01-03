0813cf75d9d76d6c77fd830ddd33a7082f49d86f
Zeshi97 <zeshishah97@gmail.com>
2021-01-03 13:43:57 -0800

    python code with connect with database

--- application.py
+++ application.py
@@ -1,6 +1,16 @@
+import os
 from flask import Flask, render_template
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
+from flask import request
 
 app=Flask(__name__)
+engine = create_engine("postgres://postgres:zeshan123@localhost:5432/mydb")
+
+db=scoped_session(sessionmaker(bind=engine))
+
+
+
 @app.route("/")
 def index():
     return render_template("home.html")
@@ -15,3 +25,22 @@ def blog():
 @app.route ("/about")
 def about():
     return render_template("about.html")
+@app.route ("/signIn")
+def signin():
+    return render_template("signIn.html")
+@app.route("/signup")
+def signup():
+    return render_template("signUp.html")
+
+@app.route("/book", methods=["POST"])
+def book():
+    name = request.form.get("name")
+    email = request.form.get("email")
+    comment = request.form.get("message")
+    db.execute("INSERT INTO friend.comment (name, email, comment) VALUES(:name, :email, :comment)",
+                {"name": name, "email": email, "comment": comment})
+    db.commit()
+    return render_template("success.html", username=name)
+
+if __name__=="__main__":
+    main()
